68cbace93fb77dd2fb9c40e96136f898746d46f6
anupsv <anupsv@users.noreply.github.com>
2023-11-16 10:15:32 +0300

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -60,24 +60,17 @@ struct EigenLayerConfig<F: BigPrimeField> {
 struct EigenLayerCircuit<F: ScalarField> {
     // constant:
     g1_one: G1Affine,
-    g2_one: G2Affine,
 
     // Public inputs:
     // 
-    quorumG1Apk: Value<G1Affine>,
-    allOperatorsCircuitG1Apk: G1Affine,
+    quorum_g1_apk: Value<G1Affine>,
+    all_operators_circuit_g1_apk: G1Affine,
     // sum of pubkeys of ALL operators (including non-signers)
     aggregate_pubkey: Value<G2Affine>,
-    taskResponseDigestBn254G2: G2Affine,
+    task_response_digest_bn254_g2: G2Affine,
     aggregate_signature: G2Affine,
     non_signer: Vec<Value<F>>,
-
-    non_signer_pubkeys: Vec<G1Affine>, 
-
-    // Public output:
-    // 
-    // H - R(alpha)[1]_1 - Z(alpha) pi_H + alpha pi'_H is a G1 point
-    verifier_point: G1Affine,
+    non_signer_pubkeys: Vec<G1Affine>
 }
 
 impl EigenLayerCircuit<Fr> {
@@ -85,22 +78,19 @@ impl EigenLayerCircuit<Fr> {
         let mut rng = rand::thread_rng(); 
         Self { 
             g1_one: G1Affine::generator(), 
-            g2_one: G2Affine::generator(), 
-            quorumG1Apk: Value::known(G1Affine::random(&mut rng)),
-            allOperatorsCircuitG1Apk: G1Affine::random(&mut rng),
+            quorum_g1_apk: Value::known(G1Affine::random(&mut rng)),
+            all_operators_circuit_g1_apk: G1Affine::random(&mut rng),
             aggregate_pubkey: Value::known(G2Affine::random(&mut rng)), 
-            taskResponseDigestBn254G2: G2Affine::random(&mut rng),
+            task_response_digest_bn254_g2: G2Affine::random(&mut rng),
             aggregate_signature: G2Affine::random(&mut rng), 
             non_signer: (0..num_non_signers).map(|i| Value::known(Fr::from(2* i as u64))).collect(), 
-            non_signer_pubkeys: (0..num_non_signers).map(|_| G1Affine::random(&mut rng)).collect(), 
-            verifier_point: G1Affine::random(&mut rng), 
+            non_signer_pubkeys: (0..num_non_signers).map(|_| G1Affine::random(&mut rng)).collect()        
         }
     }
 }
 
 
 fn main() {
-    
 
     let mut circuit = BaseCircuitBuilder::<Fr>::new(false).use_lookup_bits(22);
 
@@ -125,12 +115,12 @@ fn main() {
             .into_iter()
             .map(|point| g1_chip.assign_point::<G1Affine>(circuit.main(0), point))
             .collect();
-        let all_operators_circuit_g1_apk = g1_chip.assign_point::<G1Affine>(circuit.main(0), el_circuit.allOperatorsCircuitG1Apk);
+        let all_operators_circuit_g1_apk = g1_chip.assign_point::<G1Affine>(circuit.main(0), el_circuit.all_operators_circuit_g1_apk);
         let nonsigners_g1_apk = g1_chip.sum::<G1Affine>(circuit.main(0), g1_points.into_iter());
         signers_g1_apk = g1_chip.sub_unequal(circuit.main(0), all_operators_circuit_g1_apk, nonsigners_g1_apk, true);
 
     } else {
-        signers_g1_apk = g1_chip.assign_point::<G1Affine>(circuit.main(0), el_circuit.allOperatorsCircuitG1Apk);
+        signers_g1_apk = g1_chip.assign_point::<G1Affine>(circuit.main(0), el_circuit.all_operators_circuit_g1_apk);
     }
 
     let g1_generator = g1_chip.assign_point::<G1Affine>(circuit.main(0), el_circuit.g1_one);
@@ -139,7 +129,7 @@ fn main() {
 
     let aggregate_signature = g2_chip.assign_point::<G2Affine>(circuit.main(0), el_circuit.aggregate_signature);
     
-    let task_response_digest_bn254_g2 = g2_chip.assign_point::<G2Affine>(circuit.main(0), el_circuit.taskResponseDigestBn254G2);
+    let task_response_digest_bn254_g2 = g2_chip.assign_point::<G2Affine>(circuit.main(0), el_circuit.task_response_digest_bn254_g2);
     let multi_paired = pairing_chip.multi_miller_loop(circuit.main(0), vec![(&signers_g1_apk, &task_response_digest_bn254_g2), (&neg_rhs_g1, &aggregate_signature)]);
     let fq12_chip = Fp12Chip::new(&fq_chip);
     let result = fq12_chip.final_exp(circuit.main(0), multi_paired);
